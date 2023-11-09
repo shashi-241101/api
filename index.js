f8e15ee7a01e96f44e2be9158b8fabb5f3e4dd4e
Shashi Pandey <kr.shashi123pandey@gmail.com>
2023-11-09 19:34:48 +0530

    Auth-router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -10,9 +11,9 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(err);
 })
 const app=express();
-
-app.use("/api/user",userRouter);
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
