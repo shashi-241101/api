02876c3452f38508625b30644bc0b2cc19340491
Shashi Pandey <kr.shashi123pandey@gmail.com>
2023-11-09 19:37:21 +0530

    first commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -8,6 +8,6 @@ export const signup = async (req, res, next) => {
       await newUser.save();
       res.status(201).json('User created successfully!');
     } catch (error) {
-      console.log(error);
+      res.status(500).json(error.message);
     }
   };
