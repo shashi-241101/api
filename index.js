9cac21227a5fbc75f0d89acfbe09140daf957a13
Shashi Pandey <kr.shashi123pandey@gmail.com>
2023-11-09 14:47:17 +0530

    Mern-Estate(Backend)

---
+++ index.js
@@ -0,0 +1,18 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
+const app=express();
+
+app.use("/api/user",userRouter);
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
